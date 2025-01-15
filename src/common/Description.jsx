3fe7b83243a81c7f2a7c75bbaf9aa5eb01b15fb8
akash-535 <akashbish9e@gmail.com>
2025-01-15 11:54:00 +0530

    common class changed

--- src/common/Description.jsx
+++ src/common/Description.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Description = ({text,myClass , textTwo}) => {
+const Description = ({text,myClass , textTwo , myClassTwo}) => {
   return (
     <>  
     <p className={`text-custom-2xl font-normal leading-custom-5xl max-md:text-lg ${myClass}`}>{text}</p>
-    <p className={`text-lg font-normal leading-custom-6xl max-md:text-base ${myClass}`}>{textTwo}</p>
+    <p className={`text-lg font-normal leading-custom-6xl max-md:text-base ${myClassTwo}`}>{textTwo}</p>
     </>
     
   )
